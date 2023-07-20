16e40e76fc2ca68924d24463aedcfe4b3881c610
Admin <ashvinikushwaha189@gmail.com>
2023-07-20 22:57:24 +0530

    1.1

--- src/main/kotlin/Practical-1.1.kt
+++ src/main/kotlin/Practical-1.1.kt
@@ -1,8 +1,21 @@
 fun main()
 {
-    val a =22
-    val b =1.5
-    val cChar:Char='T'
-    println("Integer Value : $a ")
-    println("Float Value : $b ")
+    var a : Int = 22
+    var b = 1.5F //Float
+    var c : Char = 'A'
+    var d : String = "Ashvini"
+    var e : Boolean = true
+    var f : Double = 1.98987676785495
+    val g : Long = 9897849658
+    val h : Short = -9
+    val i : Byte = 111
+    println("Integer Value: $a")
+    println("Float Value: $b")
+    println("Char Value: $c")
+    println("String Value: $d")
+    println("Boolean Value: $e")
+    println("Double Value: $f")
+    println("Long Value: $g")
+    println("Short Value: $h")
+    println("Byte Value: $i")
 }
