46d13a0a7249a6ed4eb1732b69751b7a19598e69
Admin <ashvinikushwaha189@gmail.com>
2023-08-08 23:13:36 +0530

    ok

--- src/main/kotlin/Practical-1.6.kt
+++ src/main/kotlin/Practical-1.6.kt
@@ -1,13 +1,23 @@
-fun main() {
-    var a= 111
-    var b= 2222
-    var c= -222
-    var ans = a+b+c
-    var ans1 = a-b-c
-    var ans2 = a*b*c
-    var ans3 = b/a
-    println("Addition of $a $b $c is $ans")
-    println("Subtraction of $a $b $c is $ans1")
-    println("Multiplication of $a $b $c is $ans2")
-    println("Division of $b $a is $ans3")
+fun main()
+{
+    fun add(a:Int,b:Int,c:Int) {
+        var addition = a+b+c
+        println("Addition of $a $b $c is : $addition")
+    }
+    fun sub(a:Int,b:Int,c:Int){
+        var minus = a-b-c
+        println("Substraction of $a $b $c is : $minus")
+    }
+    fun mul(a:Int,b:Int,c:Int){
+        var multi = a*b*c
+        println("Multiplication of $a $b $c is : $multi")
+    }
+    fun div(a:Int,b:Int,c:Int){
+        var division = b/a
+        println("Division of $b $a is : $division")
+    }
+    add(10,20,30)
+    sub(10,20,30)
+    mul(10,20,30)
+    div(10,20,30)
 }
